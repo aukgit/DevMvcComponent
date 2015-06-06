374e6bbc2abdea6ea80bf1016ff97173d5249f1e
Alim Ul Karim <devorg.bd@gmail.com>
2015-06-06 23:50:26 +0600

    Hasher added
    Hasher added

---
+++ DevMVCComponent/DevMVCComponent/Hashing/Hasher.cs
@@ -0,0 +1,16 @@
+﻿namespace DevMVCComponent.Hashing {
+    public static class Hasher {
+        /// <summary>
+        ///     Checks nulls and returns only codes for existing ones.
+        /// </summary>
+        /// <param name="o"></param>
+        /// <returns></returns>
+        public static string Get(params object[] o) {
+            var output = "";
+            for (var i = 0; o[i] != null && i < o.Length; i++) {
+                output += o[i].GetHashCode() + "_";
+            }
+            return output.GetHashCode().ToString();
+        }
+    }
+}
